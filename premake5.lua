210a8400d6cc8c13fd99e6ea152258db2f19053b
dstacer <dstacer@gmail.com>
2020-12-26 20:34:13 -0600

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,17 +8,39 @@ project "glfw"
            "include/GLFW/glfw3native.h",
            "src/glfw_config.h",
            "src/context.c",
-           "src/init.c"
-           "src/input.c"
-           "src/monitor.c"
-           "src/vulkan.c"
+           "src/init.c",
+           "src/input.c",
+           "src/monitor.c",
+           "src/vulkan.c",
            "src/window.c"
          }
 
-   --includedirs { "%{prj.location}/src",
+   filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
 
    filter "system:windows"
-      buildoptions { "-std=c11", "-lgdi32" }
       staticruntime "On"
       systemversion "latest"
       
@@ -41,5 +63,10 @@ project "glfw"
          "_CRT_SECURE_NO_WARNINGS"
       }
    
-   filter { "system:windows", "configurations:Release" }
-      buildoptions "/MT"
+   filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
